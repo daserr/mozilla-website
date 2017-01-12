ac8d6fdece2820ca045c2c9c7b18af972f62f31f
DANIELA <daniela.serrano.p@gmail.com>
2017-01-12 15:16:50 -0500

    changed colors

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 document.querySelector('img').onclick = function() {
     alert('Ouch! Stop poking me!');
 }
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
