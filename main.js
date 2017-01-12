9d974dd7522b04f88aca5cfe836c71d571c59447
DANIELA <daniela.serrano.p@gmail.com>
2017-01-12 15:30:18 -0500

    changed image interaction

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-document.querySelector('img').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+//document.querySelector('html').onclick = function() {
+//    alert('Ouch! Stop poking me!');
+//}
 
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+    if(mySrc === 'https://pbs.twimg.com/profile_images/588130566597382145/-1EaCVVK.jpg') {
+      myImage.setAttribute ('src','https://az801952.vo.msecnd.net/uploads/cd7bc391-2913-4857-967b-6d2595284695.gif');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','https://pbs.twimg.com/profile_images/588130566597382145/-1EaCVVK.jpg');
     }
 }
+
